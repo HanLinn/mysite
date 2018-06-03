8f8e353836ae84b0118d04b864570ccee7545e8f
HanLinn <hanlinnaung87@gmail.com>
2018-06-03 20:17:42 +0630

    test

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path ('', views.index ,name='index')
+urlpatterns = [
+    path('', views.index, name='index'),
+    #127.0.0.1
+    path('<int:question_id>/', views.detail, name='detail'),
+    #127.0.0.1/1        
+    path('<int:question_id>/results/', views.results, name='result'),
+    #127.0.0.1/1/result
+    path('<int:question_id>/vote/', views.vote, name='vote')
+    #127.0.0.1/1/vote
 ]
