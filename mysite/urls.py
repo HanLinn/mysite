447de2ab516118a3700f4d61c97b452b0ffdf217
Han Linn <hanlinnaung87@gmail.com>
2018-06-13 08:42:19 +0630

    login finish

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polls import urls
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
 ]
